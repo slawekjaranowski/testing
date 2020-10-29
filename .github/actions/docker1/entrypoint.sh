9e96a250b51276384ff6c57a5382d088f878bfec
Slawomir Jaranowski <s.jaranowski@gmail.com>
2020-10-29 23:21:20 +0100

    problem matcher

--- .github/actions/docker1/entrypoint.sh
+++ .github/actions/docker1/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 echo "::add-matcher::./.github/actions/docker1/errors.json"
+
 echo "Hello"
+echo "error: error message 1"
+
+echo "Hello 1"
+echo "Hello 2"
+echo "Hello 3"
+
+echo "xxx error: error message 2"
